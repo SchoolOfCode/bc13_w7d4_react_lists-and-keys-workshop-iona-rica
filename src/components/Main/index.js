4790912f87e6d164d8ff4c0d019c2be3bd788b0b
Federica Costantino <ricacostt@gmail.com>
2022-11-10 16:25:42 +0000

    map method

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -9,7 +9,33 @@ import Post from '../Post';
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
 
-  return <main id="main"></main>;
-}
+  //map trough posts
+  //for each post return a post component
+  //id = post.postId
+
+  // const postTemplate = {
+  //   postId: 0,
+  //   title: "a title",
+  //   date: "DD/MM/YY",
+  //   author: "author",
+  //   text: lorem.generateParagraphs(7),
+  //   highlights: [
+  //     lorem.generateWords(3),
+  //     lorem.generateWords(3),
+  //     lorem.generateWords(3),
+  //   ],
+  //   image: { link: "link", alt: "alt" },
+  // };
+
+  return (
+  <Main>
+    {posts.map((postObj) => {
+    return <Post
+   id={postObj.postId} title={postObj.title} date={postObj.date} author={postObj.author} text={postObj.text} image={postObj.image}></Post>
+    })}
+
+    </Main>
+)
+  }
 
 export default Main;
